cd0a57bb9c33c02e79b476a279ce983bec712a6f
meta <eloi.fil19@hotmail.fr>
2026-05-07 09:48:09 +0200

    feat: PWA support pour Android

--- script.js
+++ script.js
@@ -177,3 +177,14 @@ document.getElementById('imageModal').onclick = (e) => {
         document.getElementById('imageModal').classList.add('hidden');
     }
 };
+
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js').then(registration => {
+            console.log('ServiceWorker registration successful');
+        }).catch(err => {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
